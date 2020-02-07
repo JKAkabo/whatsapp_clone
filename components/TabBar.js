432fce9cd79ae49f4c89afac9f731c9732e3938e
John Kennedy <developer.akabojohnkennedy@gmail.com>
2020-02-07 08:38:37 +0000

    Initial Commit

---
+++ components/TabBar.js
@@ -0,0 +1,32 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            initialRouteName="Feed"
+            tabBarOptions={{
+                activeTintColor: '#e91e63',
+                labelStyle: { fontSize: 12 },
+                style: { backgroundColor: 'powderblue' },
+            }}
+        >
+            <Tab.Screen
+                name="Feed"
+                // component={Feed}
+                options={{ tabBarLabel: 'Home' }}
+            />
+            <Tab.Screen
+                name="Notifications"
+                // component={Notifications}
+                options={{ tabBarLabel: 'Updates' }}
+            />
+            <Tab.Screen
+                name="Profile"
+                // component={Profile}
+                options={{ tabBarLabel: 'Profile' }}
+            />
+        </Tab.Navigator>
+    );
+}
